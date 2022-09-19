268a03fc2c774345af05f1bd2a4cec48509ba1ef
suraj0798 <surajdhas07@gmail.com>
2022-09-19 13:40:27 +0530

    added cover to songs

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ console.log("wrlcome to letsplay")
 
 // initialize the variables
 let songIndex = 0;
-let audioElement = new Audio("_Arcando - When I'm With You [NCS Release].mp3");
+let audioElement = new Audio("songs/_Arcando - When I'm With You [NCS Release].mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
+let songitem = Array.from(document.getElementsByClassName("songitem"));
 let songs = [
-    {songName: "_Arcando - When I'm With You [NCS Release]", filePath: "songs/_Arcando - When I'm With You [NCS Release].mp3", coverPath: "covers/1.jpg"},
-    {songName: "_Mblue - Follow Me [NCS Release]", filePath: "songs/_Mblue - Follow Me [NCS Release].mp3", coverPath: "covers/2.jpg"},
-    {songName: "No Lie", filePath: "songs/_PLEEG - Home [NCS Release].mp3", coverPath: "covers/3.jpg"},
-    {songName: "No Lie", filePath: "songs/32Stitches & CHENDA, Harley Bird - Freedom [NCS Release].mp3", coverPath: "covers/4.jpg"},
-    {songName: "No Lie", filePath: "songs/BEAUZ - Illusion (feat. Crunr) [NCS Release].mp3", coverPath: "covers/5.jpg"},
-    {songName: "No Lie", filePath: "songs/Facading - Tonight [NCS Release].mp3", coverPath: "covers/6.jpg"},
-    {songName: "No Lie", filePath: "songs/JOXION - Talk That Way [NCS Release].mp3", coverPath: "covers/7.jpg"},
-    {songName: "No Lie", filePath: "songs/Lost Identities x Robbie Rosen - Moments (Acoustic Version) [NCS Release].mp3", coverPath: "covers/8.jpg"},
-    {songName: "No Lie", filePath: "songs/Tollef - Like A Stone [NCS Release].mp3", coverPath: "covers/9.jpg"},
-    {songName: "No Lie", filePath: "songs/WBN x Mojnz - Radio [NCS Release].mp3", coverPath: "covers/10.jpg"},
+    {songName: "_Arcando - When I'm With You [NCS Release]", filePath: "songs/_Arcando - When I'm With You [NCS Release].mp3", coverPath: "covers/_Arcando - When I'm With You [NCS Release].jpg"},
+    {songName: "_Mblue - Follow Me [NCS Release]", filePath: "songs/_Mblue - Follow Me [NCS Release].mp3", coverPath: "covers/_Mblue - Follow Me [NCS Release].jpg"},
+    {songName: "_PLEEG - Home [NCS Release]", filePath: "songs/_PLEEG - Home [NCS Release].mp3", coverPath: "covers/_PLEEG - Home [NCS Release].jpg"},
+    {songName: "32Stitches & CHENDA, Harley Bird - Freedom [NCS Release]", filePath: "songs/32Stitches & CHENDA, Harley Bird - Freedom [NCS Release].mp3", coverPath: "covers/32Stitches & CHENDA, Harley Bird - Freedom [NCS Release].jpg"},
+    {songName: "BEAUZ - Illusion (feat. Crunr) [NCS Release]", filePath: "songs/BEAUZ - Illusion (feat. Crunr) [NCS Release].mp3", coverPath: "covers/BEAUZ - Illusion (feat. Crunr) [NCS Release].jpg"},
+    {songName: "Facading - Tonight [NCS Release]", filePath: "songs/Facading - Tonight [NCS Release].mp3", coverPath: "covers/Facading - Tonight [NCS Release].jpg"},
+    {songName: "JOXION - Talk That Way [NCS Release]", filePath: "songs/JOXION - Talk That Way [NCS Release].mp3", coverPath: "covers/JOXION - Talk That Way [NCS Release].jpg"},
+    {songName: "Lost Identities x Robbie Rosen - Moments (Acoustic Version) [NCS Release]", filePath: "songs/Lost Identities x Robbie Rosen - Moments (Acoustic Version) [NCS Release].mp3", coverPath: "covers/Lost Identities x Robbie Rosen - Moments (Acoustic Version) [NCS Release].jpg"},
+    {songName: "Tollef - Like A Stone [NCS Release]", filePath: "songs/Tollef - Like A Stone [NCS Release].mp3", coverPath: "covers/Tollef - Like A Stone [NCS Release].jpg"},
+    {songName: "WBN x Mojnz - Radio [NCS Release]", filePath: "songs/WBN x Mojnz - Radio [NCS Release].mp3", coverPath: "covers/WBN x Mojnz - Radio [NCS Release].jpg"},
   
 ]
+
+songitem.forEach((element, i) => {
+    console.log(element, i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
 //play/pause click
 masterPlay.addEventListener("click",()=>{
